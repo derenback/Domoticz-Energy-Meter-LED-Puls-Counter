145a749a01e0e8bb33ad4061f708b6cb487ba4f4
David Derenbäck <derenback@gmail.com>
2021-01-23 11:24:31 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Domoticz-Energy-Meter-LED-Puls-Counter
 Domoticz plugin for counting LED pulses on your energy meter using a pin on the GPIO port.
+
+## Requirements
+- LED detector connected to the GPIO port of the raspberry pi that Domoticz is running on.
+
+## Installation
+```bash
+cd ~/domoticz/plugins
+git clone https://github.com/derenback/Domoticz-Energy-Meter-LED-Puls-Counter.git
+sudo apt install python3-rpi.gpio
+sudo systemctl restart domoticz
+```
+- Make sure to have the setting "Accept new Hardware Devices" turned on for new devices to be added when adding the Hardware in domoticz.
+
+## Update
+```bash
+cd ~/domoticz/plugins/Domoticz-Energy-Meter-LED-Puls-Counter
+git pull
+sudo systemctl restart domoticz
+```
+
+## Tested on
+- Domoticz version: 2020.2 (build 11997)
+- Meter, Network Energy Service type 83334-3IKBAA (Bi-directional) and 83330-3IHA (Single direction) from Eon
+
+## Version history
+    0.1.0 Initial version
+
---
+++ plugin.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+"""
+Energy Meter LED Puls Counter via GPIO
+Author: Derenback
+Requirements:
+    1. SMA Sunny Tripower or Sunny Boy with Modbus TCP enabled.
+    2. python 3.x
+    3. pip3 install -U pymodbus pymodbusTCP
+
+"""
+"""
+<plugin key="EnergyLEDCount" name="Energy Meter LED Puls Counter via GPIO" version="0.0.1" author="Derenback">
+    <params>
+        <param field="Mode1" label="GPIO pin" width="40px" required="true" default="8" />
+        <param field="Mode2" label="Report interval sec." width="40px" required="true" default="20" />
+        <param field="Mode3" label="Debug" width="75px">
+            <options>
+                <option label="On" value="Debug"/>
+                <option label="Off" value="Off" default="true" />
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
+import Domoticz
+import RPi.GPIO as GPIO 
+import time
+
+pulse_count = 0
+pulse_count_total = 0
+time_for_last_update = time.time()
+
+def led_pulse_callback(channel):
+    global pulse_count
+    pulse_count += 1
+    if (Parameters["Mode3"] == "Debug"):
+        Domoticz.Log("EnergyLEDCount, callback, pulses: " + str(pulse_count))
+
+
+def onStart():
+    Domoticz.Log("Energy Meter LED Puls Counter via GPIO plugin started")
+
+    if (Parameters["Mode3"] == "Debug"):
+        Domoticz.Log("Debug is On")
+        Domoticz.Log("Heartbeat time: " + Parameters["Mode2"])
+        Domoticz.Log("RPi.GPIO imported, Version: "+str(GPIO.VERSION)+", Raspberry Pi board revision: "+str(GPIO.RPI_INFO['P1_REVISION']))
+
+    if 1 not in Devices:
+        Domoticz.Device(Name="Usage", Unit=1, TypeName="kWh", Used=1).Create()
+
+    PIN_LED_IN = int(Parameters["Mode1"])
+
+    GPIO.setmode(GPIO.BOARD)  
+    GPIO.setup(PIN_LED_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(PIN_LED_IN, GPIO.FALLING, callback=led_pulse_callback, bouncetime=50)
+    if (Parameters["Mode3"] == "Debug"):
+        Domoticz.Log("EnergyLEDCount, GPIO setup done")
+
+    Domoticz.Heartbeat(int(Parameters["Mode2"]))
+    if (Parameters["Mode3"] == "Debug"):
+        Domoticz.Log("EnergyLEDCount, Heartbeat set to " + Parameters["Mode2"])
+
+def onHeartbeat():
+    global pulse_count, pulse_count_total, time_for_last_update
+    pulse_count_total += pulse_count
+    wattage = 0
+    if (pulse_count != 0):
+        wattage = int(3600 / ((time.time() - time_for_last_update) / pulse_count))
+    Devices[1].Update(0, str(wattage) + ";" + str(pulse_count_total))
+    if (Parameters["Mode3"] == "Debug"):
+        Domoticz.Log("EnergyLEDCount, Time: " + str(round(time.time() - time_for_last_update,5)))  
+        Domoticz.Log("EnergyLEDCount, Wattage: " + str(wattage) + " Pulses total: " + str(pulse_count_total))
+
+    time_for_last_update = time.time()
+    pulse_count = 0
+    
+
+def onStop():
+    GPIO.cleanup()
+
